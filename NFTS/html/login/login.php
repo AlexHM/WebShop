932aa00c5e119c6b09773367710bfc58f9af48d6
AlexHM <alejandro.honrubia@live.u-tad.com>
2021-11-07 20:53:12 +0100

    Added signUP with email verification

--- NFTS/html/login/login.php
+++ NFTS/html/login/login.php
@@ -1,33 +1,23 @@
 <?php
 
 require("../../connection/connection.php");
-$email= $_POST["email"];
-$pass= $_POST["password"];
+require("../../connection/searchUser.php");
+
 
 
 
 try {
     
 
-$query = "select * from users where email= :email and password= :password";
-$result= $db->prepare($query);
-
-$email2=htmlentities(addslashes($email));
-$pass2=htmlentities(addslashes($pass));
-
-$result->bindValue(":email",$email2);
-$result->bindValue(":password",$pass2);
-
-$result->execute();
 
 if ($result->rowCount() > 0) {
 
     if (isset($_POST["remember"])) {
     header("location: ../home/home.html");
-    echo "recuerda sesion";       
+         
     }else{
         header("location: ../home/home.html");
-        echo "no recuerda sesion"; 
+         
     }
     
 }else{
