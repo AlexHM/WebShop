4ab9f71ad5f225af4567161db03e935cf2a7262d
Ángel Blanco Rodríguez <73697059+AngelBlanco97@users.noreply.github.com>
2021-11-27 01:31:10 +0100

    finished home.php
    home style finished, just need php to bring information from database

--- NFTS/html/home/home.php
+++ NFTS/html/home/home.php
@@ -58,13 +58,13 @@ if ($flagUser) {
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="#" alt="login">Login</a>
+                        <a class="nav-link" href="#">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="#" alt="signup">Sign up</a>
+                        <a class="nav-link" href="#">Sign up</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="#" alt="shoppingCardImg">Shop Card</a>
+                        <a class="nav-link" href="#">Shop Card</a>
                     </li>
                 </ul>
                 <form class="d-flex">
@@ -75,12 +75,19 @@ if ($flagUser) {
         </div>
     </nav>
     
+<<<<<<< Updated upstream
+=======
+    <!--spacing-->
+    <div style="height:35px;"></div>
+
+>>>>>>> Stashed changes
     <div role="alert" aria-live="assertive" aria-atomic="true" class="toast position-fixed bottom-0 end-0" data-bs-autohide="false" style="z-index: 9999">
         <div class="toast-header bg-warning">
             <strong class="me-auto">ENFFY said: </strong>
             <small>just now</small>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
+<<<<<<< Updated upstream
         <div class="toast-body">
             Bienvenido a nuestro catalogo! Puedes seleccionar los productos y meterlos en tu carrito pulsando en el boton<strong>"Add to card"</strong>. Si necesitas ayuda, no dudes en consultar con nosotros.
         </div>
@@ -252,16 +259,84 @@ if ($flagUser) {
 
 
         <main class="col-9" >
+=======
+        <div class="toast-body">Bienvenido a nuestro catalogo! Puedes seleccionar los productos y meterlos en tu carrito pulsando en el boton<strong>"Add to card"</strong>. Si necesitas ayuda, no dudes en consultar con nosotros.</div>
+    </div>
+
+
+    <!--Div filters in searchs products-->
+
+    <div class="container">
+        <div class="col-12">
+            <div class="display-4 border-bottom ">Filters</div>
+            <div style="height:15px;"></div>
+            <fieldset class="row border-top-1">
+                <div class="col-md-4 col-sm-12 d-flex align-items-center justify-content-around">
+                    <div class="input-group d-flex align-items-center">
+                        <label class="input-group-text bg-warning border-warning" for="inputGroupSelect01">Options</label>
+                        <select class="form-select border-warning" id="inputGroupSelect01">
+                            <option selected>Choose one...</option>
+                            <option value="1">Most likes</option>
+                            <option value="3">Most views</option>
+                            <option value="4">Newest</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 d-flex align-items-center">
+                    <div class="input-group d-flex justify-content-around">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input bg-warning border-warning" type="checkbox" role="switch" id="flexSwitchCheckChecked">
+                            <label class="form-check-label" for="flexSwitchCheckChecked">GIF</label>
+                        </div>
+                        <div class="form-check form-switch align-items-center">
+                            <input class="form-check-input bg-warning border-warning" type="checkbox" role="switch" id="flexSwitchCheckChecked">
+                            <label class="form-check-label " for="flexSwitchCheckChecked">PNG</label>
+                        </div>
+                        <div class="form-check form-switch ">
+                            <input class="form-check-input bg-warning border-warning" type="checkbox" role="switch" id="flexSwitchCheckChecked" >
+                            <label class="form-check-label" for="flexSwitchCheckChecked">JPG</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4  col-sm-12 display-flex align-items-center">
+                    <div class="input-group d-flex align-items-center">
+                        <label class="input-group-text bg-warning border-warning" for="inputGroupSelect01">Max Price</label>
+                        <select class="form-select border-warning" id="inputGroupSelect01">
+                            <option selected>Choose one...</option>
+                            <option value="1">&lt; 100€</option>
+                            <option value="3">&lt; 200€</option>
+                            <option value="4">&lt; 500€</option>
+                            <option value="5">&lt; 1.000€</option>
+                            <option value="6">&lt; 5.000€</option>
+                            <option value="7">&lt; 10.000€</option>
+                            <option value="7">&gt; 10.000€</option>
+                        </select>
+                    </div>
+                </div>
+            </fieldset>
+            <div class="display-8 border-bottom text-white">.</div>
+        </div>  
+    </div>
+    <div style="height:50px;"></div>
+    <div>
+        <main class="col-12 " >
+>>>>>>> Stashed changes
             <?php
             require("../../connection/nameProduct.php");
             require("../../connection/imageProduct.php");
             require("../../connection/descriptionProduct.php");
             require("../../connection/priceProduct.php");
-
-
             ?>
+<<<<<<< Updated upstream
             <div id="products" class="container-fluid">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
+=======
+            
+            <div id="products" class="container">
+                <div class="display-4 border-bottom ">Products</div>
+                <div style="height:30px;"></div>
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
+>>>>>>> Stashed changes
                     <div class="col">
                         <div class="card h-100">
                             <img src="<?php echo 'data:image/png; base64,' . base64_encode($images[0]); ?>" class="card-img-top" alt="Imagen Producto">
@@ -456,20 +531,43 @@ if ($flagUser) {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
 
 =======
     
+=======
+    
+    <!--SCRIPT TO SHOW TOAST TO INFORMATE CLIENT HOW TO ADD PRODUTS-->
+>>>>>>> Stashed changes
     <script>
         window.onload = (event) =>{
             let myAlert = document.querySelector('.toast');
             let bsAlert = new bootstrap.Toast(myAlert);
             bsAlert.show();
+<<<<<<< Updated upstream
             
         }
     </script>
     <!-- "<img src='data:image/png; base64," . base64_encode($images[1]) . "' id='imgBlob' alt='image1'>"-->
 >>>>>>> Stashed changes
+=======
+        }
+    </script>
+>>>>>>> Stashed changes
 </body>
-
+<footer>
+    <div class="container">
+        <footer class="py-3 my-2">
+            <ul class="nav justify-content-center border-top">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+            </ul>
+            <p class="text-center text-muted">&copy; 2021 Enffy, Inc</p>
+        </footer>
+    </div>
+</footer>
 </html>
