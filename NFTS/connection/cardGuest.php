c015da29c9042109cbc43b5c84635f340798d1ad
AlexHM <alejandro.honrubia@live.u-tad.com>
2021-12-08 13:51:01 +0100

    print shopping card items

--- NFTS/connection/cardGuest.php
+++ NFTS/connection/cardGuest.php
@@ -3,30 +3,50 @@ require("connection.php");
 
 
 
+
 $guest =0;
+$user ="";
+
+if (isset($_SESSION["ses_user"])) {
+    $user = $_SESSION["ses_user"];
+}
 if (isset($_COOKIE["cok_guest"])) {
     $guest = $_COOKIE["cok_guest"];
 }
 
 try {
-    $queryCardGuest = "select id_product,quantity from bought_products where id_guest = :id_guest";
-    $resultCardGuest = $db->prepare($queryCardGuest);
-    $resultCardGuest->bindValue(":id_guest",$guest);
-    $resultCardGuest->execute();
+    
+    $selectProductsBuy = "";
+    if (isset($_COOKIE["cok_guest"])) {
+        $selectProductsBuy = "select * from bought_products B join products P on B.id_product = P.id where id_guest= :id_guest";
+        $resultJoinCard = $db->prepare($selectProductsBuy);
+        $resultJoinCard->bindValue(":id_guest",$_COOKIE["cok_guest"]);
+    }else{
+       
+        $selectProductsBuy = "select * from bought_products B join products P on B.id_product = P.id where email_user= :email";
+        $resultJoinCard = $db->prepare($selectProductsBuy);
+        $resultJoinCard->bindValue(":email",$user);
+        $resultJoinCard->execute();
+    }
+   
 
-    $id_productG = array();
-    $quantityG = array();
-    $countG = 0;
-
-  
-    while ($rowG = $resultCardGuest->fetch(PDO::FETCH_ASSOC)) {
-        $id_productG[$countG] = $rowG['id_product'];
-        $quantityG[$countG] = $rowG['quantity'];
-        $countG = $countG + 1;
+    $id_productB = array();
+    $nameB = array();
+    $descriptionB = array();
+    $quantityB = array();
+    $priceB = array();
+    $imageB = array();
+    $countB = 0;
+    while ($rowB = $resultJoinCard->fetch(PDO::FETCH_ASSOC)) {
+        $id_productB[$countB] = $rowB['id_product'];
+        $nameB[$countB] = $rowB['name'];
+        $descriptionB[$countB] = $rowB['description'];
+        $priceB[$countB] = $rowB['price'];
+        $imageB[$countB] = $rowB['image'];
+        $quantityB[$countB] = $rowB['quantity'];
+        $countB = $countB + 1;
     }
 
 } catch (\Exception $th) {
     echo "Error: " . $th;
 }
-
-?>
