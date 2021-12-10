532ac32c2b5216ce346d9be7214201c5ffb57d92
Alejandro <alejandro.honrubia@live.u-tad.com>
2021-12-10 10:00:53 +0100

    Fixed destroySession$Cookie

--- NFTS/cookies/destroySession.php
+++ NFTS/cookies/destroySession.php
@@ -3,7 +3,7 @@ session_start();
 session_destroy();
 
 unset($_COOKIE['cok_card_user']); 
-setcookie("cok_card_user", "", time() + -1, "/");
+setcookie("cok_user_card", "", time() + -1, "/");
 
 unset($_COOKIE['cok_user']); 
 setcookie("cok_user", "", time() + -1, "/");
